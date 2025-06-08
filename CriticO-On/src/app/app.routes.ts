f07d1ca39ac539c1658243c70fd31e758914bdf3
Julisa Figueroa <julisa.figueroa2101@alumnos.ubiobio.cl>
2025-06-07 20:53:10 -0400

    html y css de registrar y iniciar sesion

--- CriticO-On/src/app/app.routes.ts
+++ CriticO-On/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componentesAuth/login/login.component';
+import { SigninComponent } from './componentesAuth/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent},
+    { path: 'signin', component: SigninComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
