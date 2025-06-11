6a3b56c8a80b88f1031fc169aa63fb3223a6ef16
Julisa Figueroa <julisa.figueroa2101@alumnos.ubiobio.cl>
2025-06-11 12:02:16 -0400

    cerrar sesion cuando identifica usuario registrado

--- CriticO-On/src/app/components/header/header.component.ts
+++ CriticO-On/src/app/components/header/header.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OffcanvasComponent } from "../offcanvas/offcanvas.component";
+import { AuthServiceService } from '../../services/serviceAuth/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [OffcanvasComponent],
+  imports: [OffcanvasComponent, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isAuthenticated = false;
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private authService: AuthServiceService ){}
+
+  ngOnInit(): void {
+    this.authService.authStatus$.subscribe(status => {
+      this.isAuthenticated = status;
+    });
+  }
 
   goToLogin() {
     this.router.navigate(['/login']);
@@ -19,5 +28,10 @@ export class HeaderComponent {
 
   goToSignIn() {
     this.router.navigate(['/signin']);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
