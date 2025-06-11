169a477426c5230ca2d05b921d804368896e6c64
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-06-11 11:47:46 -0400

    Crear review y content detail

---
+++ CriticO-On/src/app/components/createreview/createreview.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { ContentdetailComponent } from "../contentdetail/contentdetail.component";
+import { NgIf, NgFor, NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-createreview',
+  standalone: true,
+  imports: [ContentdetailComponent,NgFor, FormsModule, NgClass],
+  templateUrl: './createreview.component.html',
+  styleUrl: './createreview.component.css'
+})
+export class CreatereviewComponent {
+  rating: number = 10;
+
+  // Devuelve arreglo de 10 elementos con valores true (llena) o false (vacía)
+  get starsArray(): boolean[] {
+    const full = Math.floor(this.rating);
+    return Array.from({ length: 10 }, (_, i) => i < full);
+  }
+
+  get firstRow(): boolean[] {
+    return this.starsArray.slice(0, 5);
+  }
+
+  get secondRow(): boolean[] {
+    return this.starsArray.slice(5, 10);
+  }
+
+  onRatingInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = parseFloat(input.value);
+  
+    if (isNaN(value)) {
+      this.rating = 0;
+    } else if (value > 10) {
+      this.rating = 10;
+    } else if (value < 0) {
+      this.rating = 0;
+    } else {
+      this.rating = value;
+    }
+  
+    // Forzar visualmente el valor corregido en el input
+    input.value = this.rating.toString();
+  }
+  
+
+}
