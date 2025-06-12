d29e6d1509b3ccee620a7cd458320e2f578fd9e4
patito31 <antonia.yevenes1901@alumnos.ubiobio.cl>
2025-06-11 20:59:33 -0400

    arreglo ajdhjgkjdf

--- backend-server/routes/contentRoutes.js
+++ backend-server/routes/contentRoutes.js
@@ -5,7 +5,8 @@ const contentController = require('../controllers/contentController');
 router.get('/serie_pelicula', contentController.getSeriesPeliculas);
 router.get('/review', contentController.getReviews);
 router.get('/populares', contentController.getPopulares);
-router.get('/:id', contentController.getContenidoById);
 router.get('/recientes', contentController.getRecientes);
+router.get('/:id', contentController.getContenidoById);
+
 
 module.exports = router;
