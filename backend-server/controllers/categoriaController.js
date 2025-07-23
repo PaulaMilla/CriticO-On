32043c6a7de53f5f27b3ff9245914e29012ace8f
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-07-23 13:00:30 -0400

    cambios waaa

--- backend-server/controllers/categoriaController.js
+++ backend-server/controllers/categoriaController.js
@@ -4,12 +4,17 @@ exports.obtenerPeliculasPorGenero = (req, res) => {
     const generoNombre = req.params.genero;
 
     const query = `
-        SELECT sp.* 
+        SELECT 
+            sp.*, 
+            IFNULL(AVG(r.rating), 0) AS promedio_rating
         FROM serie_pelicula sp
         JOIN sp_genero sg ON sp.id_sp = sg.fk_sp
         JOIN genero g ON sg.fk_genero = g.id_genero
+        LEFT JOIN review_sp rsp ON rsp.fk_sp = sp.id_sp
+        LEFT JOIN review r ON r.id_review = rsp.fk_review
         WHERE g.nombre = ?
-        ORDER BY rating DESC
+        GROUP BY sp.id_sp
+        ORDER BY promedio_rating DESC;
     `;
 
     mysql.query(query, [generoNombre], (error, results) => {
